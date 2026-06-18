a3836f69586e23b1ed688963bc26307e9f5f47d5
artessgoat <artessgoat@gmail.com>
2026-06-18 17:13:47 +0700

    Version 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let secretNumber = Math.floor(Math.random()*100)+ 1;
+let secretNumber = Math.floor(Math.random()*6)+ 1;
   console.log("เลขปัจจุบัน:", secretNumber);
 //let secretnumber คือเริ่มระบบสุ่มเลข และconsole คือโค้ดยืนยันว่าเลขสุ่มแล้ว 
 document.getElementById("btn").onclick = function(){
@@ -10,19 +10,19 @@ document.getElementById("btn").onclick = function(){
   //ข้างบนนี่คือโค้ดตำแหน่งรับข้อมูลตัวเลข
   let result = document.getElementById("result");
   if (guess < secretNumber) {
-    result.textContent = "น้อยไป";
+    result.textContent = "เลขน้อยไป";
   }
   else if (guess > secretNumber) {
-    result.textContent = "มากไป";
+    result.textContent = "เลขมากไป";
   }
   else {
-    result.textContent = "ถูกต้อง! กดเริ่มใหม่เพื่อเริ่มสุ่มเลขถัดไป";
+    result.textContent = "คุณพบแล้ว! 🍀 กดเริ่มใหม่เพื่อเริ่มสุ่มเลขถัดไป";
       document.getElementById("btn").disabled = true;
 }
   //สามโค้ดข้างบนคือผลลัพธ์ที่เกิดขึ้นเมื่อทายตัวเลข มีตั้งแต่ น้อยกว่า มากกว่า และถูกต้อง หลังทายถูกแล้ว ปุ่มทายจะถูกปิดลงด้วยคำสั่ง disabled = true เพื่อให้กดผปุ่มเริ่มใหม่แล้วระบบจะกลับมาทำงานอีกครั้ง
   
   document.getElementById("restart").onclick = function () {
-    secretNumber = Math.floor(Math.random()*100) +1;
+    secretNumber = Math.floor(Math.random()*6) +1;
     document.getElementById("btn").disabled = false;
     result.textContent = "";
     document.getElementById("guess").Value = "";
